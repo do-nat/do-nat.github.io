ce29f6dbd6db6e494f7ca16afcae8b49ff1e55ba
do-nat <47325790+do-nat@users.noreply.github.com>
2021-06-16 12:30:31 +0200

    deploy

---
+++ precache-manifest.506a41174aeb2c79c7af686a97776de7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbc77860e830df711f7",
+    "url": "/css/app.beac714c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "421b3871427aba324f4ffe494d1c5a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdccab027fafc252018a",
+    "url": "/js/about.ff32cdf2.js"
+  },
+  {
+    "revision": "adbc77860e830df711f7",
+    "url": "/js/app.acbab33e.js"
+  },
+  {
+    "revision": "75b09a5e502370d3e9c2",
+    "url": "/js/chunk-vendors.11a3d692.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
