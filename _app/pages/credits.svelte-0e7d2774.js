52ac5e0e6ad7d762f0381df9780a2ec205d26e82
do-nat <47325790+do-nat@users.noreply.github.com>
2022-07-20 10:33:02 +0000

    Deploying to main from @ do-nat/pages@65dd9fdf1485227da522a1f829c04dea1bb20f73 🚀

--- _app/pages/credits.svelte-0e7d2774.js
+++ _app/pages/credits.svelte-0e7d2774.js
@@ -1,4 +1,4 @@
-import{S as L,i as g,s as k,k as q,e as h,t as d,P as A,d as r,n as S,c as m,a as u,g as f,b as e,f as D,I as l,J as j}from"../chunks/vendor-b0b7d0f4.js";const F=!0,H=!1;function J(I){let c,t,n,_,v,s,p,w,a,y;return{c(){c=q(),t=h("div"),n=h("h1"),_=d("Credits"),v=d(`
+import{S as L,i as g,s as k,k as q,e as h,t as d,P as A,d as r,n as S,c as m,a as u,g as f,b as e,f as D,I as l,J as j}from"../chunks/vendor-85e653e2.js";const F=!0,H=!1;function J(I){let c,t,n,_,v,s,p,w,a,y;return{c(){c=q(),t=h("div"),n=h("h1"),_=d("Credits"),v=d(`
 	Icons made by`),s=h("a"),p=d("DinosoftLabs"),w=d(`
 	from `),a=h("a"),y=d("www.flaticon.com'"),this.h()},l(o){A('[data-svelte="svelte-1eoangi"]',document.head).forEach(r),c=S(o),t=m(o,"DIV",{id:!0,class:!0});var i=u(t);n=m(i,"H1",{class:!0});var x=u(n);_=f(x,"Credits"),x.forEach(r),v=f(i,`
 	Icons made by`),s=m(i,"A",{href:!0,title:!0,class:!0});var C=u(s);p=f(C,"DinosoftLabs"),C.forEach(r),w=f(i,`
